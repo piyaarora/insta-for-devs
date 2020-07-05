dba7e42cd4683b98f52cf654d61a8ed8a62397a8
piyaarora <arorapiya141@gmail.com>
2020-07-05 20:47:41 +0530

    done with authentication

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI: 'mongodb+srv://Priya:Hello@world10@cluster0-qb5s1.mongodb.net/instafordevs?retryWrites=true&w=majority'
+    MONGOURI: 'mongodb+srv://Priya:Hello@world10@cluster0-qb5s1.mongodb.net/instafordevs?retryWrites=true&w=majority',
+    JWT_SECRET: 'goodluck'
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config')
+const requireLogin = require('../middleware/requireLogin')
 
-router.get('/', (req, res) => {
-    res.send("hello")
+
+router.get('/protected', requireLogin, (req, res) => {
+    res.send("hello user ")
 })
 
 router.post('/signup', (req, res) => {
@@ -20,24 +25,55 @@ router.post('/signup', (req, res) => {
                 return res.status(422).json({ error: "user already exists" })
 
             }
-            const user = new User({
-                email,
-                password,
-                name
-            })
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        email,
+                        password: hashedpassword,
+                        name
+                    })
 
-            user.save()
-                .then(user => {
-                    res.json({ message: "user saved successfully" })
+                    user.save()
+                        .then(user => {
+                            res.json({ message: "user saved successfully" })
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                })
+        })
+
+        .catch(err => {
+            console.log(err)
+        })
+    res.json({ message: "successfully posted" })
+})
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.status(422).json({ error: "please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid email or password" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET)
+                        res.json({ token: token })
+                        res.json({ message: "successfully signed in" })
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid email or password" })
+                    }
                 })
                 .catch(err => {
                     console.log(err)
                 })
         })
-        .catch(err => {
-            console.log(err)
-        })
-    res.json({ message: "successfully posted" })
 })
 
 module.exports = router
